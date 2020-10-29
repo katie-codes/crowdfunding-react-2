372a9c064d38c556698a0974200e10eb387a27a8
katie-codes <katiepattersondesign@gmail.com>
2020-10-29 12:39:03 +1000

    retry commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
     const [projectList, setProjectList] = useState([]);
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}projects/`, {
+        fetch(`${process.env.REACT_APP_API_URL}/projects/`, {
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json"
