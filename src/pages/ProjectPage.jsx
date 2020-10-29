ce9850ba81f56544809a9840a74ee5abc5638f86
katie-codes <katiepattersondesign@gmail.com>
2020-10-29 18:06:26 +1000

    project page update

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -29,9 +29,6 @@ function ProjectPage() {
       });
   }, [id]);
 
-
-  console.log(Object.values(projectData)[0].id)
-
   const formattedProjectRes = Object.values(projectData)[0]
   const formattedProjectSum = Object.values(projectData)[1]
 
