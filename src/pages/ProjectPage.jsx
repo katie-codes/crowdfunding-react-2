c3e8af0891860dee12982ee2f7b12621be526163
katie-codes <katiepattersondesign@gmail.com>
2020-10-29 15:15:54 +1000

    new project page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,3 +1,6 @@
+Projects page
+
+
 
 
 
@@ -5,35 +8,57 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function ProjectPage() {
-    const [projectData, setProjectData] = useState({ pledges: [] });
-    const { id } = useParams();
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`)
-        .then((results) => {
-            return results.json();
-        })
-        .then((data) => {
-            setProjectData(data);
-        });
-    }, []);
-    return (
-        <div>
-            <h2>{projectData.title}</h2>
-            <h3>Created at: {projectData.date_created}</h3>
-            <h3>{`Status: ${projectData.is_open}`}</h3>
-            <h3>Pledges:</h3>
-            {(projectData.pledges) ?
+  const [projectData, setProjectData] = useState({ projects: [], pledges: [] });
+  const { id } = useParams();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token")
+
+    fetch(`${process.env.REACT_APP_API_URL}/projects/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+    })
+
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setProjectData(data);
+      });
+  }, [id]);
+
+
+  console.log(Object.values(projectData)[0].id)
+
+  const formattedProjectRes = Object.values(projectData)[0]
+  const formattedProjectSum = Object.values(projectData)[1]
+
+  return (
+    <div>
+      <h2>{formattedProjectRes.title}</h2>
+      <h3>Created: {formattedProjectRes.date_created}</h3>
+      <h3>Project Type: {formattedProjectRes.category}</h3>
+      <h3>Goal: {formattedProjectRes.goal}</h3>
+      <h3>{`Status: ${formattedProjectRes.is_open}`}</h3>
+      <p>{formattedProjectRes.description}</p>
+      <h3>Goal: {formattedProjectRes.owner}</h3>
+      <h3>Pledges:</h3>
+      {(formattedProjectRes && formattedProjectRes.pledges) ?
         <ul>
-            {projectData.pledges.map((pledgeData, key) => {
+          {formattedProjectRes.pledges.map((pledgeData, key) => {
             return (
-                <li key={key}>
-                    ${pledgeData.amount} from {pledgeData.supporter_id}
-                </li>
-                );
-            })}
-        </ul>: null }
-            <h5>This is still the PROJECT PAGE.jsx</h5>
-        </div>
-    );
+              <li key={key}>
+                ${pledgeData.amount} from {pledgeData.supporter_id}
+              </li>
+            );
+          })}
+        </ul> : null}
+    </div>
+
+  );
 }
+
 export default ProjectPage;
+
